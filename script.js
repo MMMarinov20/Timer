9fd6976b4ae50eca21cd4ed681074bb676c99ebd
MMMarinov20 <MMMarinov20@codingburgas.bg>
2021-07-17 14:47:49 +0300

    Add detecting changes function

--- script.js
+++ script.js
@@ -18,3 +18,41 @@ function soundAlarm() {
         setTimeout(playSound, 1200 * i);
     }
 }
+
+function updateValue(key, value) {
+    if(value < 0) {
+        value = 0;
+        console.log("Positive numbers only");
+    }
+
+
+    if(key == "seconds") {
+        if(value < 10) {
+            value = "0" + value;
+        }
+
+        if(value > 59) {
+            value = 59;
+        }
+    }
+
+    $("#" + key).html(value || 0);
+    timerObj[key] = value;
+
+
+    console.log("Min", timerObj.minutes);
+    console.log("Sec", timerObj.seconds);
+}
+
+(function detectChanges(key) {
+    let input = "#" + key + "-input";
+
+    $(input).change(function() {
+        updateValue(key, $(input).val());
+    });
+
+    $(input).keyup(function() {
+        updateValue(key, $(input).val());
+    });
+    return arguments.callee;
+})("minutes")("seconds");
